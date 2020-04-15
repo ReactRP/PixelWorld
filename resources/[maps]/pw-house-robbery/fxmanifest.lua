5a0e7430daa194116c8d92c8834ab209c9da0d69
Chris Rogers <chris@pixelworldrp.com>
2020-04-15 19:30:11 +0100

    Half the maps fucking done :D

---
+++ resources/[maps]/pw-house-robbery/fxmanifest.lua
@@ -0,0 +1,14 @@
+description 'PixelWorld House Robbery'
+name 'PixelWorld pw-house-robbery'
+author 'PixelWorldRP [Ultrunz] - https://pixelworldrp.com'
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'bodacious'
+games {'gta5'}
