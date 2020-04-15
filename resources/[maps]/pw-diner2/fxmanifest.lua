c7bff6784576aee5bfdf87336045f09d96ccb303
Chris Rogers <chris@pixelworldrp.com>
2020-04-15 19:29:03 +0100

    Cars

---
+++ resources/[maps]/pw-diner2/fxmanifest.lua
@@ -0,0 +1,14 @@
+description 'PixelWorld Pops Diner'
+name 'PixelWorld pw-diner2'
+author 'PixelWorldRP [Ultrunz] - https://pixelworldrp.com'
+
+files {
+    "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'bodacious'
+games {'gta5'}
