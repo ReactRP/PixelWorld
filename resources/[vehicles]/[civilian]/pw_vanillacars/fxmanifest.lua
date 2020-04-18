de75b09c681748df05592dc6e304376e16b84093
Dr Nick <drnick7000@gmail.com>
2020-04-18 20:51:57 +0100

    Hopefully remove weapon rewards from used vehicles

--- resources/[vehicles]/[civilian]/pw_vanillacars/fxmanifest.lua
+++ resources/[vehicles]/[civilian]/pw_vanillacars/fxmanifest.lua
@@ -3,12 +3,12 @@ name 'PixelWorld pw_vanillacars'
 author 'PixelWorldRP [Panda_builds] - https://pixelworldrp.com'
 
 files {
-
     'handling.meta',
-
+    'vehicles.meta',
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 
 
 fx_version 'adamant'
