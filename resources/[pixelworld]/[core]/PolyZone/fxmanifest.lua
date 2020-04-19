05069bb1cfb6217dfef7d9433ed748427b49c9d4
creaKtive <creaktive.design@gmail.com>
2020-04-19 22:12:20 +0100

    PolyZones

---
+++ resources/[pixelworld]/[core]/PolyZone/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'bodacious'
+games {'gta5'} -- 'gta5' for GTAv / 'rdr3' for Red Dead 2, 'gta5','rdr3' for both
+
+server_scripts {
+    "commands.lua",
+    "server.lua"
+}
+
+client_scripts {
+    "client.lua",
+    "creation.lua"
+}
