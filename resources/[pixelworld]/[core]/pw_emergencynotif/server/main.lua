6ff87573d5da67b8dd0cbc643993479fe08b258f
Chris Rogers <chris@pixelworldrp.com>
2020-04-29 11:04:00 +0100

    Fixers

--- resources/[pixelworld]/[core]/pw_emergencynotif/server/main.lua
+++ resources/[pixelworld]/[core]/pw_emergencynotif/server/main.lua
@@ -7,9 +7,12 @@ end)
 RegisterServerEvent('pw_notify:server:sendEmergencySpecificJobs')
 AddEventHandler('pw_notify:server:sendEmergencySpecificJobs', function(jobs, code, message)
     if jobs and type(jobs) == "table" and code and message then
+        local query
         MySQL.Async.fetchAll("SELECT * FROM `alert_codes` WHERE `code_id` = @code", {['@code'] = code}, function(alert)
             if alert[1] ~= nil then
+                query = "UPDATE `alert_codes` SET `timesRequested` = `timesRequested` + 1 WHERE `record_id` = @record"
                 if alert[1].sendAlert then
+                    query = "UPDATE `alert_codes` SET `timesRequested` = `timesRequested` + 1, `timesSent` = `timesSent` + 1 WHERE `record_id` = @record"
                     for k, v in pairs(jobs) do
                         local players = exports['pw_core']:getDutyPlayers(v)
                         for t, q in pairs(players) do
@@ -17,6 +20,9 @@ AddEventHandler('pw_notify:server:sendEmergencySpecificJobs', function(jobs, cod
                         end
                     end
                 end
+                MySQL.Sync.execute(query, {['@record'] = alert[1].record_id})
+            else
+                print(' ^1[ERROR] - An incorrect dispatch code was submitted - L25 SERVER.LUA | Code: '..code)
             end
         end)
     else
@@ -27,9 +33,12 @@ end)
 RegisterServerEvent('pw_notify:server:sendEmergencyAll')
 AddEventHandler('pw_notify:server:sendEmergencyAll', function(code, message)
     if code and message then
+    local query
         MySQL.Async.fetchAll("SELECT * FROM `alert_codes` WHERE `code_id` = @code", {['@code'] = code}, function(alert)
             if alert[1] ~= nil then
+                query = "UPDATE `alert_codes` SET `timesRequested` = `timesRequested` + 1 WHERE `record_id` = @record"
                 if alert[1].sendAlert then
+                    query = "UPDATE `alert_codes` SET `timesRequested` = `timesRequested` + 1, `timesSent` = `timesSent` + 1 WHERE `record_id` = @record"
                     if alert[1].jobs ~= nil then
                         local jobs = json.decode(alert[1].jobs)
                         if type(jobs) == "table" then
@@ -42,9 +51,12 @@ AddEventHandler('pw_notify:server:sendEmergencyAll', function(code, message)
                         end
                     end
                 end
+                MySQL.Sync.execute(query, {['@record'] = alert[1].record_id})
+            else
+                print(' ^1[ERROR] - An incorrect dispatch code was submitted - L54 SERVER.LUA | Code: '..code)
             end
         end)
     else
-        print(' ^1[ERROR] - You have not specified the correct syntax to submit an emergency alert - L48 SERVER.LUA')
+        print(' ^1[ERROR] - You have not specified the correct syntax to submit an emergency alert - L58 SERVER.LUA')
     end
 end)
