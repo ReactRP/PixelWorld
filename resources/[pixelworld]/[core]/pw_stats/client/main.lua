c44a0eb0d6dfeb252d3058170654f6eab520ac30
Dr Nick <drnick7000@gmail.com>
2020-04-30 16:55:37 +0100

    Item Updates

--- resources/[pixelworld]/[core]/pw_stats/client/main.lua
+++ resources/[pixelworld]/[core]/pw_stats/client/main.lua
@@ -44,3 +44,79 @@ AddEventHandler('pw_stats:client:doQuickSpeedBoost', function(energy, length)
         SetRunSprintMultiplierForPlayer(player, 1.00)
     end
 end)
+
+RegisterNetEvent('pw_stats:client:doNeedsBoostForItem')
+AddEventHandler('pw_stats:client:doNeedsBoostForItem', function(itemLabel, needsBoost, itemHealth)
+    if characterLoaded then
+        local progTime = 10
+        local progText = 'Using ' .. itemLabel
+        if needsBoost.wait.time ~= nil then
+            progTime = needsBoost.wait.time
+            if needsBoost.wait.text ~= nil then
+                progText = needsBoost.wait.text
+            end
+        end
+        if needsBoost.anim ~= nil then
+            TriggerEvent('pw_stats:client:doItemAnim', needsBoost.anim, (needsBoost.animLength ~= nil and needsBoost.animLength or 20))
+        end        
+        if progTime > 0 then
+            TriggerEvent('pw:progressbar:progress',
+            {
+                name = 'stats_use_item_prog',
+                duration = (progTime * 1000),
+                label = progText,
+                useWhileDead = false,
+                canCancel = false,
+                controlDisables = {
+                    disableMovement = false,
+                    disableCarMovement = false,
+                    disableMouse = false,
+                    disableCombat = false,
+                },
+            },
+            function(status)
+                if not status then
+                    UpdateNeedsBoost(needsBoost, itemHealth)
+                end
+            end)
+        else
+            UpdateNeedsBoost(needsBoost, itemHealth)
+        end
+    end
+end)
+
+function UpdateNeedsBoost(needsBoost, itemHealth)
+    for k, v in pairs(needsBoost) do
+        if k == 'add' or k == 'remove' then
+            for t, q in pairs(v) do
+                print(k, t, q)
+                if itemHealth < 10 and t == 'hunger' then
+                    exports.pw_notify:SendAlert('error', 'You Ate Old Food and Feel Your Stomach Hurting.', 4000)
+                    TriggerEvent('pw_needs:client:updateNeeds', 'remove', 'hunger', 10)
+                    doMouldyFoodHealthDecrease()
+                elseif itemHealth < 10 and t == 'thirst' then
+                    exports.pw_notify:SendAlert('error', 'You Drank Something Old and Feel Your Stomach Hurting.', 4000)
+                    TriggerEvent('pw_needs:client:updateNeeds', 'remove', 'thirst', 10)
+                    doMouldyFoodHealthDecrease()
+                else
+                    TriggerEvent('pw_needs:client:updateNeeds', k, t, q)
+                end
+            end    
+        end
+        if k == 'drugs' then
+            for t, q in pairs(v) do
+                TriggerEvent('pw_needs:client:updateDrugs', t, q)
+            end
+        end
+    end
+    if needsBoost.speedBoost ~= nil and needsBoost.speedBoost.len ~= nil and needsBoost.speedBoost.energy ~= nil then
+        TriggerEvent('pw_stats:client:doQuickSpeedBoost', needsBoost.speedBoost.energy, needsBoost.speedBoost.len)
+    end
+end
+
+function doMouldyFoodHealthDecrease()
+    local playerPed = PlayerPedId()
+    local currentHealth = GetEntityHealth(playerPed)
+    SetEntityHealth(playerPed, (currentHealth - 10))
+end
+
--- resources/[pixelworld]/[core]/pw_stats/server/server.lua
+++ resources/[pixelworld]/[core]/pw_stats/server/server.lua
@@ -11,29 +11,11 @@ AddEventHandler('pw_core:itemUsed', function(_src, data)
     if needsBoost ~= nil and type(needsBoost) == "table" then
         if needsBoost.add ~= nil or needsBoost.remove ~= nil or needsBoost.drugs ~= nil or needsBoost.speedBoost ~= nil then
             TriggerClientEvent('pw_discord:client:overRide', _src, "Consuming "..data.label)
-            for k, v in pairs(needsBoost) do
-                if k == 'add' or k == 'remove' then
-                    for t, q in pairs(v) do
-                        print(k, t, q)
-                        TriggerClientEvent('pw_needs:client:updateNeeds', _src, k, t, q)
-                    end    
-                end
-                if k == 'drugs' then
-                    for t, q in pairs(v) do
-                        TriggerClientEvent('pw_needs:client:updateDrugs', _src, t, q)
-                    end
-                end
-            end
-            if needsBoost.anim ~= nil then
-                TriggerClientEvent('pw_stats:client:doItemAnim', _src, needsBoost.anim, (needsBoost.animLength ~= nil and needsBoost.animLength or 20))
-            end
-            if needsBoost.speedBoost ~= nil and needsBoost.speedBoost.len ~= nil and needsBoost.speedBoost.energy ~= nil then
-                TriggerClientEvent('pw_stats:client:doQuickSpeedBoost', _src, needsBoost.speedBoost.energy, needsBoost.speedBoost.len)
-            end
             --Item Removal
             _char:Inventory():Remove().Slot(data.slot, 1, function(done)
                 
             end)
+            TriggerClientEvent('pw_stats:client:doNeedsBoostForItem', _src, data.label, needsBoost, data.health)
         end
     end
 end)
