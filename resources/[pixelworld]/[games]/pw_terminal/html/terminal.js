d7f739f757773d2e20a636d43ebc77e0ebd2653c
creaKtive <creaktive.design@gmail.com>
2020-05-01 02:59:46 +0100

    Terminal updates

--- resources/[pixelworld]/[games]/pw_terminal/html/terminal.js
+++ resources/[pixelworld]/[games]/pw_terminal/html/terminal.js
@@ -2,40 +2,46 @@ $(function () {
     window.addEventListener('message', function (event) {
         if (event.data.type == "enableui") {
             document.body.style.display = event.data.enable ? "block" : "none";
+            if (event.data.enable) $("#terminalInput").focus();
         }
     });
 });
 
+var hacking = false;
+var hackerName = ['Tr1N1ty', 'N3O', 'M0rph3us', 't4Nk', 'b1sh0p', '0r4c13', 'sw1tX', 'cYPh3r', 'ap0c', 'd0z3R'];
 var virusName = ["1337", "t0xic", "ph4ntom", "4LPhA", "v1rus", "biTe", "krypt0", "cyb3r", "Bi0", "ac1d", "gh0st", "L0rd", "r4dical", "PWNER", "H4X0R", "buRn", "MuX", "d3st0y3r", "phr34k", "Pl4gu3", "0verrid3", "Ch40s"];
-var activeVirus = virusName[Math.floor((Math.random()*virusName.length))];
+var activeVirus = virusName[Math.floor((Math.random() * virusName.length))];
 var ranIntervals = 0
 var PIDs = new Array();
-PIDs[0] = randomPID(1000,4000);
-PIDs[1] = randomPID(PIDs[0],6000);
-PIDs[2] = randomPID(PIDs[1],8000);
-PIDs[3] = randomPID(PIDs[2],9999);
+PIDs[0] = randomPID(1000, 4000);
+PIDs[1] = randomPID(PIDs[0], 6000);
+PIDs[2] = randomPID(PIDs[1], 8000);
+PIDs[3] = randomPID(PIDs[2], 9999);
 
-setInterval(function() {
-    var prevVirus = activeVirus;
-    activeVirus = virusName[Math.floor((Math.random()*virusName.length))];
-    delete Object.assign(directories[".root"].content, {[activeVirus + ".sh"]: directories[".root"].content[prevVirus + ".sh"] })[prevVirus + ".sh"];
-    ranIntervals++;
-    var oldPid = PIDs[3];
-    PIDs[3] = randomPID(oldPid, oldPid + (1000 * ranIntervals));
+setInterval(function () {
+    if (!hacking) {
+        var prevVirus = activeVirus;
+        activeVirus = virusName[Math.floor((Math.random() * virusName.length))];
+        delete Object.assign(directories[".root"].content, { [activeVirus + ".sh"]: directories[".root"].content[prevVirus + ".sh"] })[prevVirus + ".sh"];
+        ranIntervals++;
+        var oldPid = PIDs[3];
+        PIDs[3] = randomPID(oldPid, oldPid + (1000 * ranIntervals));
+    }
 }, 120 * 1000);
 
 var commandList = {
-    bash:       "executes commands read from a file",
-    cat:        "cat [filename] will print the contents of that file.",
-    cd:         "cd [directory] will switch current directory to [directory].",
-    clear:      "clears all text in the terminal.",
-    help:       "lists possible terminal commands.",
-    history:    "lists the command line history.",
-    logout:     "exits a login shell.",
-    ls:         "lists information about files and directories.",
-    man:        "describes a command.",
-    ps:         "lists the current processes.",
-    uname:      "prints information about the machine and operating system it is run on."
+    bash: "executes commands read from a file",
+    cat: "cat [filename] will print the contents of that file.",
+    cd: "cd [directory] will switch current directory to [directory].",
+    clear: "clears all text in the terminal.",
+    hack: "",
+    help: "lists possible terminal commands.",
+    history: "lists the command line history.",
+    logout: "exits a login shell.",
+    ls: "lists information about files and directories.",
+    man: "describes a command.",
+    ps: "lists the current processes.",
+    uname: "prints information about the machine and operating system it is run on."
 };
 
 var directories = {
@@ -77,22 +83,23 @@ var directories = {
     }
 };
 
-
-var user = "root@pixelworld.com: ~";
+var defaultUser = "root@pixelworld.com: ~";
+var user = defaultUser;
 var curDirectory = "home";
 var commandHistory = [];
 var commandIndex = -1;
 
-function randomPID(min,max) {
+function randomPID(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 function closeTerminal() {
     document.body.style.display = 'none';
     $.post('http://pw_terminal/escape', JSON.stringify({}));
+    location.reload();
 }
 
-$(document).on('click','[data-act=closeWindow]',function(){
+$(document).on('click', '[data-act=closeWindow]', function () {
     closeTerminal()
 });
 
@@ -121,7 +128,7 @@ $(document).ready(function () {
             var secondary = input.split(" ")[1];
             if (
                 (commandList[command] === undefined &&
-                command != "continue") &&
+                    command != "continue") &&
                 command
             ) {
                 replaceInput();
@@ -140,6 +147,9 @@ $(document).ready(function () {
                 return;
             }
             switch (command) {
+                case "hack":
+                    hack();
+                    break;
                 case "bash":
                     if (secondary) runFile(secondary);
                     break;
@@ -192,8 +202,92 @@ $(document).ready(function () {
         }
     }
 
+    var c = 0
+    var txt = [
+        'FORCE: XX0022. ENCYPT://000.222.2345',
+        'TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1',
+        'RETRY: REINDEER FLOTILLA',
+        'X:> /PIXELWORLD/GAMES/FIVEM/ EXECUTE -PLAYERS 128',
+        '================================================',
+        'Priority 1 // local / scanning...',
+        'scanning ports...',
+        'BACKDOOR FOUND (23.45.23.12:30120)',
+        'BACKDOOR FOUND (13.66.23.12:30130)',
+        'BACKDOOR FOUND (13.66.23.12:30140)',
+        '...',
+        '...',
+        'BRUTE.EXE -r -z',
+        '...locating vulnerabilities...',
+        '...vulnerabilities found...',
+        'MCP/> DEPLOY CLU',
+        'SCAN: __ 0100.0000.0554.0080',
+        'SCAN: __ 0020.0000.0553.0080',
+        'SCAN: __ 0001.0000.0554.0550',
+        'SCAN: __ 0012.0000.0553.0030',
+        'SCAN: __ 0100.0000.0554.0080',
+        'SCAN: __ 0020.0000.0553.0080',
+        'FFF BJDY **** ++home hack',
+        'BACKDOOR FOUND (23.45.23.12:30120)',
+        'BACKDOOR FOUND (13.66.23.12:30130)',
+        'BACKDOOR FOUND (13.66.23.12:30140)',
+        '=============================================',
+        '...',
+        '...',
+        'BRUTE.EXE -r -z',
+        '...locating vulnerabilities...',
+        '...vulnerabilities found...',
+        'ACCESS GRANTED.'
+    ]
+
+    var curIndex = 0
+    var curIteration = 0
+
+    function typeName() {
+        setTimeout(function() {
+            if (curIteration > (txt[curIndex].length - 1)) {
+                if((curIndex + 1) > (txt.length - 1)) {
+                    curIndex = 0;
+                    curIteration = 0;
+                    replaceInput();
+                    addInput();
+                    hacking = false;
+                } else {
+                    curIndex++;
+                    curIteration = 0;
+                    $('#terminalOutput').append("<br>");
+                    $('#terminalOutput').append(txt[curIndex][curIteration]);
+                    curIteration++;
+                    document.getElementById('terminalOutput').scrollTop = 9999999;
+                    typeName();
+                }
+            } else {
+                $('#terminalOutput').append(txt[curIndex][curIteration]);
+                curIteration++;
+                typeName();
+            }
+        }, 20);
+    }
+
+    function hack() {
+        if (!hacking) {
+            hacking = true;
+            var bg = document.getElementById("terminal");
+            var input = document.getElementById("terminalInput");
+            var output = document.getElementById("terminalOutput");
+            var closeButton = document.getElementById("closeButton");
+            bg.style.background = "#000000";
+            input.style.color = "#20C20E";
+            output.style.color = "#20C20E";
+            closeButton.style.background = "#4F781E";
+            user = hackerName[Math.floor((Math.random() * hackerName.length))] + "@p1x3Lw0RLd.c0M: ~";
+            clear();
+            replaceInput();
+            typeName();
+        }
+    }
+
     function runFile(file) {
-        if ((curDirectory && directories[curDirectory].content[file] && directories[curDirectory].content[file].type === "exe") || 
+        if ((curDirectory && directories[curDirectory].content[file] && directories[curDirectory].content[file].type === "exe") ||
             (curDirectory === "" && directories[file] && directories[file].type === "exe")) {
             //run file
         } else if ((curDirectory && directories[curDirectory].content[file] === undefined) || (curDirectory === "" && directories[file] === undefined)) {
@@ -202,8 +296,8 @@ $(document).ready(function () {
                 '"' + file + '"' + ' file not found.<br>'
             );
             addInput();
-        } else if ((curDirectory && directories[curDirectory].content[file] && directories[curDirectory].content[file].type !== "exe") || 
-                    (curDirectory === "" && directories[file] && directories[file].type !== "exe")) {
+        } else if ((curDirectory && directories[curDirectory].content[file] && directories[curDirectory].content[file].type !== "exe") ||
+            (curDirectory === "" && directories[file] && directories[file].type !== "exe")) {
             var fType = (curDirectory ? directories[curDirectory].content[file].type : directories[file].type)
             switch (fType) {
                 case "text":
@@ -254,7 +348,7 @@ $(document).ready(function () {
 
         var found = false;
 
-        Object.keys(directories).forEach(function(key) {
+        Object.keys(directories).forEach(function (key) {
             if (key === input) {
                 found = true;
                 switchDirectory(key)
@@ -318,7 +412,7 @@ $(document).ready(function () {
 
     function printList(list) {
         replaceInput();
-        Object.keys(commandList).forEach(function(key) {
+        Object.keys(commandList).forEach(function (key) {
             $("#terminalOutput").append(key + "<br>");
         });
         addInput();
@@ -327,7 +421,7 @@ $(document).ready(function () {
     function printFiles(all) {
         replaceInput();
         if (curDirectory) {
-            Object.keys(directories[curDirectory].content).forEach(function(key) {
+            Object.keys(directories[curDirectory].content).forEach(function (key) {
                 if (all === true || (all === false && directories[curDirectory].content[key].visible)) {
                     var color = ""
                     switch (directories[curDirectory].content[key].type) {
@@ -346,7 +440,7 @@ $(document).ready(function () {
                 }
             });
         } else {
-            Object.keys(directories).forEach(function(key) {
+            Object.keys(directories).forEach(function (key) {
                 if (all === true || (all === false && directories[key].visible)) {
                     var color = ""
                     switch (directories[key].type) {
@@ -372,7 +466,7 @@ $(document).ready(function () {
     function replaceInput() {
         var value = $("#terminalInput").val();
         $("#terminalInput").remove();
-        $("#terminalOutput").append(value + "<br>");
+        $("#terminalOutput").append((value || "") + "<br>");
     }
 
     function addInput() {
@@ -428,7 +522,7 @@ $(document).ready(function () {
             fileList = input[0] === "man" ? commandList : (curDirectory ? directories[curDirectory].content : directories);
         }
         if (input.length === 2 && input[1] != "") {
-            Object.keys(fileList).forEach(function(file) {
+            Object.keys(fileList).forEach(function (file) {
                 if (file.substring(0, input[1].length) === input[1]) {
                     validList.push(file);
                 }
@@ -447,12 +541,12 @@ $(document).ready(function () {
                 );
             }
         } else if (command.length) {
-            Object.keys(commandList).forEach(function(option) {
+            Object.keys(commandList).forEach(function (option) {
                 if (option.substring(0, input[0].length) === input[0]) {
                     validList.push(option);
                 }
             });
-            Object.keys((curDirectory ? directories[curDirectory].content : directories)).forEach(function(option) {
+            Object.keys((curDirectory ? directories[curDirectory].content : directories)).forEach(function (option) {
                 if (option.substring(0, input[0].length) === input[0]) {
                     validList.push(option);
                 }
