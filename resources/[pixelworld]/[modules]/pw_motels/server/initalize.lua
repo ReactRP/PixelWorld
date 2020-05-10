f784fb5be7050d53b6dbb77b9a74054eef245a06
Chris Rogers <chris@pixelworldrp.com>
2020-05-10 22:53:09 +0100

    Meh

--- resources/[pixelworld]/[modules]/pw_motels/server/initalize.lua
+++ resources/[pixelworld]/[modules]/pw_motels/server/initalize.lua
@@ -1,3 +1,7 @@
+PW = nil
+motelRooms = {}
+rooms = {}
+
 TriggerEvent('pw:loadFramework', function(framework)
     PW = framework
 end)
@@ -9,37 +13,39 @@ AddEventHandler('pw:databaseCachesLoaded', function(caches)
     for k, v in pairs(rooms) do
         motelRooms[v.room_id] = loadMotelRoom(v.room_id)
     end
-end)
 
-PW.RegisterServerCallback('pw_motels:server:requestMotels', function(source, cb)
-    cb(motels)
-end)
+    PW.RegisterServerCallback('pw_motels:server:requestMotels', function(source, cb)
+        cb(motels)
+    end)
 
-PW.RegisterServerCallback('pw_motels:server:requestRooms', function(source, cb)
-    cb(rooms)
-end)
-
-PW.RegisterServerCallback('pw_motels:server:requestMotelRooms', function(source, cb, motelId)
-    local fuckshitdicktwat = {}
-    for k, v in pairs(rooms) do
-        if v.motel_id == motelId and v.motel_type == "Door" then
-            table.insert(fuckshitdicktwat, v)
+    PW.RegisterServerCallback('pw_motels:server:requestMotelRooms', function(source, cb, motelId)
+        local fuckshitdicktwat = {}
+        for k, v in pairs(rooms) do
+            if v.motel_id == motelId and v.motel_type == "Door" then
+                table.insert(fuckshitdicktwat, v)
+            end
         end
-    end
-    cb(fuckshitdicktwat)
-end)
+        cb(fuckshitdicktwat)
+    end)
 
-PW.RegisterServerCallback('pw_motels:server:receiveComplexes', function(source, cb)
-    local tbl = {}
-    local tbl2 = {}
-    for k, v in pairs(motels) do 
-        table.insert(tbl, {['name'] = v.name, ['coords'] = json.decode(v.location), ['motel_id'] = v.motel_id})
-    end
-    repeat Wait(0) until motelRooms ~= nil
-    for t, q in pairs(motelRooms) do
-        rooms[q.roomID] = q.requestClient()
-    end
-    cb(tbl, tbl2)
+    PW.RegisterServerCallback('pw_motels:server:receiveComplexes', function(source, cb)
+        local tbl = {}
+        local tbl2 = {}
+        for k, v in pairs(motels) do 
+            table.insert(tbl, {['name'] = v.name, ['coords'] = json.decode(v.location), ['motel_id'] = v.motel_id})
+        end
+
+        for t, q in pairs(rooms) do
+            
+            if q.motel_type == "Teleport" then
+                local roomMeta = json.decode(q.roomMeta)
+                tbl2[q.room_id] = { ['room_id'] = q.room_id, ['locked'] = roomMeta.doorLocked, ['motel_name'] = motels[q.motel_id].name, ['motel_id'] = q.motel_id, ['room_number'] = q.room_number, ['motel_type'] = q.motel_type, ['teleport_meta'] = json.decode(q.teleport_meta), ['inventories'] = json.decode(q.inventories), ['occupied'] = q.occupied, ['occupier'] = q.occupier, ['occupierCID'] = q.occupierCID, ['charSpawn'] = q.charSpawn, ['roomMeta'] = q.roomMeta}
+            else
+                tbl2[q.room_id] = { ['room_id'] = q.room_id, ['motel_name'] = motels[q.motel_id].name, ['motel_id'] = q.motel_id, ['mainEntrance'] = json.decode(q.mainEntrance), ['room_number'] = q.room_number, ['motel_type'] = q.motel_type, ['inventories'] = json.decode(q.inventories), ['occupied'] = q.occupied, ['occupier'] = q.occupier, ['occupierCID'] = q.occupierCID, ['charSpawn'] = q.charSpawn, ['roomMeta'] = q.roomMeta}
+            end
+        end
+        cb(tbl, tbl2)
+    end)
 end)
 
 function assignRoom(src, cid)
@@ -81,4 +87,4 @@ end)
 
 exports('unassignRoom', function(src, cid)
     unassignRoom(src, cid)
-end) 
+end)
