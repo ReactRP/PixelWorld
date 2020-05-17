ef8a1f1e1755d882c3e3a67bf35193a8e226607b
creaKtive <creaktive.design@gmail.com>
2020-05-17 02:01:39 +0100

    Added Remove().ByName item removal

--- resources/[pixelworld]/[core]/pw_core/server/wrappers/character.lua
+++ resources/[pixelworld]/[core]/pw_core/server/wrappers/character.lua
@@ -1118,6 +1118,44 @@ function loadCharacter(source, steam, cid)
                         end
                     end
 
+                    remove.ByName = function(item, amt, cb)
+                        local doCb, done = nil, false
+                        local total = (amt and tonumber(amt) or 1)
+                        MySQL.Async.fetchScalar('SELECT SUM(count) FROM `stored_items` WHERE `inventoryType` = 1 AND `identifier` = @id AND item = @item', { ['item'] = item, ['id'] = self.cid }, function(has)
+                            if has then
+                                if(has >= total)then
+                                    MySQL.Async.fetchAll('SELECT * FROM `stored_items` WHERE `inventoryType` = 1 AND `identifier` = @id AND item = @item', { ['item'] = item, ['id'] = self.cid }, function(itemSlots)
+                                        for k,v in pairs(itemSlots) do
+                                            local processed = false
+                                            if total <= 0 then
+                                                done = true
+                                                processed = true
+                                            elseif v.count <= total then
+                                                MySQL.Async.execute("DELETE FROM `stored_items` WHERE `record_id` = @rec", {['@rec'] = v.record_id}, function()
+                                                    total = total - v.count
+                                                    processed = true
+                                                end)
+                                            elseif v.count > total then
+                                                local remaining = v.count - total
+                                                MySQL.Async.execute("UPDATE `stored_items` SET `count` = @remain WHERE `record_id` = @rec", { ['@remain'] = remaining, ['@rec'] = v.record_id }, function()
+                                                    total = 0
+                                                    processed = true
+                                                end)
+                                            end
+                                            repeat Wait(0) until processed == true
+                                            if done then break; end
+                                            if cb then cb(true); end
+                                        end
+                                    end)
+                                else
+                                    if cb then cb(false); end
+                                end
+                            else
+                                if cb then cb(false); end
+                            end
+                        end)                            
+                    end
+
                     remove.Slot = function(slot, qty, cb)
                         if slot then
                             MySQL.Async.fetchAll("SELECT * FROM `stored_items` WHERE `inventoryType` = 1 AND `identifier` = @cid AND `slot` = @slot", { ['@cid'] = self.cid, ['@slot'] = slot}, function(selectedItem)
