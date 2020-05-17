68af554c1f2e90b1208a696ec1cda61adf65b14c
creaKtive <creaktive.design@gmail.com>
2020-05-17 02:36:59 +0100

    Bombs + Bomb defusal converted

---
+++ resources/[pixelworld]/[modules]/pw_bombs/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games {'gta5'}
+
+description 'PixelWorld Bombs'
+name 'PixelWorld: pw_bombs'
+author 'PixelWorldRP [creaKtive & Chris Rogers] - https://pixelworldrp.com'
+version 'v1.0.0'
+
+server_scripts {
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
